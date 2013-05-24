a02f2d8e1747c981662623cc364e84be61efd3b2
Javier Mendiara Cañardo <javier@tid.es>
2013-05-24 10:53:23 +0200

    ADD grunt-release to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,6 +58,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-release');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
